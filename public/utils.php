ac70f814d3417eba12d26ef7b4a598eab402c208
Alex Rich <arich@hmc.edu>
2021-08-16 20:00:46 -0700

    add missing file

---
+++ public/utils.php
@@ -0,0 +1,9 @@
+<?php 
+
+function cleanName($name) {
+    $start = strlen('img/IMG_');
+    $prefixRemoved = substr($name, $start);
+    return substr($prefixRemoved, 0, strpos($prefixRemoved, "."));
+}
+
+?>
